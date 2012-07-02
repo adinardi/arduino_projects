a34519bbc6af394f61a596fe4b6d444abfe022d3
Angelo DiNardi <angelo@dinardi.name>
2012-07-02 01:46:49 -0700

    Latest software for light control - add touchscreen switch.

--- room_lighting/room_lighting.ino
+++ room_lighting/room_lighting.ino
@@ -3,8 +3,14 @@
 #define BLUEPIN 11
 #define LIGHT1PIN 12
 #define SWITCHPIN 2
+#define TOUCH_Y1 A2
+#define TOUCH_X2 A3
+#define TOUCH_Y2 A4
+#define TOUCH_X1 A5
 
 int r = 0;
+int onState = 0;
+int processedStateChange = 0;
 
 void setup()
 {
@@ -18,9 +24,9 @@ void setup()
   pinMode(SWITCHPIN, INPUT);
   digitalWrite(LIGHT1PIN, LOW);
 
-  analogWrite(REDPIN, 255);
-  analogWrite(GREENPIN, 255);
-  analogWrite(BLUEPIN, 255);
+  analogWrite(REDPIN, 0);
+  analogWrite(GREENPIN, 0);
+  analogWrite(BLUEPIN, 0);
 }
 
 void loop()
@@ -39,9 +45,94 @@ void loop()
 //    delay(5);
 //  }
 
-  if(digitalRead(SWITCHPIN) == HIGH) {
-    digitalWrite(LIGHT1PIN, HIGH);
-  } else {
-    digitalWrite(LIGHT1PIN, LOW);
+//  Serial.print("X:");
+//  Serial.print(getTouchX());
+//  Serial.print(", Y:");
+//  Serial.println(getTouchY());
+//  boolean hasTouch = (getTouchX() || getTouchY());
+  boolean hasTouch = (getTouchY() > 50);
+  if (hasTouch && processedStateChange == 0) {
+    processedStateChange = 1;
+    if (onState == 0) {
+      onState = 1;
+      digitalWrite(LIGHT1PIN, HIGH);
+      analogWrite(REDPIN, 255);
+      analogWrite(GREENPIN, 255);
+      analogWrite(BLUEPIN, 255);
+    } else if (onState == 1) {
+      onState = 0;
+      digitalWrite(LIGHT1PIN, LOW);
+      analogWrite(REDPIN, 0);
+      analogWrite(GREENPIN, 0);
+      analogWrite(BLUEPIN, 0);
+    }
+  } else if (!hasTouch) {
+     processedStateChange = 0;
   }
+  
+  delay(100);
+
+//  if (digitalRead(SWITCHPIN) == HIGH && processedStateChange == 0) {
+//    delay(5);
+//    if (digitalRead(SWITCHPIN) == LOW) {
+//      return;
+//    }
+//    processedStateChange = 1;
+//    if (onState == 0) {
+//      onState = 1;
+//      digitalWrite(LIGHT1PIN, HIGH);
+//      analogWrite(REDPIN, 255);
+//      analogWrite(GREENPIN, 255);
+//      analogWrite(BLUEPIN, 255);
+//    } else if (onState == 1) {
+//      onState = 0;
+//      digitalWrite(LIGHT1PIN, LOW);
+//      analogWrite(REDPIN, 0);
+//      analogWrite(GREENPIN, 0);
+//      analogWrite(BLUEPIN, 0);
+//    }
+//  } else if (digitalRead(SWITCHPIN) == LOW) {
+//    processedStateChange = 0; 
+//  }
+}
+
+void fadeUpLEDs() {
+  for (r = 10; r <= 255; r++) {
+    analogWrite(REDPIN, r);
+    analogWrite(GREENPIN, r);
+    analogWrite(BLUEPIN, r);
+    delay(1);
+  }
+}
+
+int getTouchX() {
+  pinMode(TOUCH_X1, OUTPUT);
+  pinMode(TOUCH_X2, OUTPUT);
+  pinMode(TOUCH_Y1, INPUT);
+  pinMode(TOUCH_Y2, INPUT);
+  
+  digitalWrite(TOUCH_Y1, LOW);
+  digitalWrite(TOUCH_Y2, LOW);
+  
+  digitalWrite(TOUCH_X1, HIGH);
+  digitalWrite(TOUCH_X2, LOW);
+  
+  delay(10);
+  return analogRead(TOUCH_Y2);
+}
+
+int getTouchY() {
+  pinMode(TOUCH_Y1, OUTPUT);
+  pinMode(TOUCH_Y2, OUTPUT);
+  pinMode(TOUCH_X1, INPUT);
+  pinMode(TOUCH_X2, INPUT);
+  
+  digitalWrite(TOUCH_X1, LOW);
+  digitalWrite(TOUCH_X2, LOW);
+  
+  digitalWrite(TOUCH_Y1, HIGH);
+  digitalWrite(TOUCH_Y2, LOW);
+  
+  delay(10);
+  return analogRead(TOUCH_X1);
 }
