7b182c5487011bd795e58c9b3fa3c44456643a13
Angelo DiNardi <angelo@dinardi.name>
2012-07-02 01:47:16 -0700

    Improve reliability of door lock.
    This seems to help keep the lock from getting in a state where it fails to unlock.
    It still seems to get to a point where it unlocks twice successively (servo on for 4 seconds instead of 2).
    No unlock failures (so it seems).

--- rfid_door_lock/rfid_door_lock.ino
+++ rfid_door_lock/rfid_door_lock.ino
@@ -20,11 +20,16 @@ SoftwareSerial BlueTooth(PIN_BT_RX, PIN_BT_TX);
 
 Servo door;
 
-int  val = 0; 
-char code[10]; 
+int  val = 0;
+char code[10];
 String tag = "";
 int bytesread = 0;
 int isOpen = 0;
+
+bool isReadingTag = false;
+long doorDebounceDelay = 100;
+long lastDoorDebounceTime = 0;
+int lastDoorState = HIGH;
 
 void setup() {
   pinMode(PIN_DOOR_CONTROL, OUTPUT); // Door controller
@@ -32,75 +37,67 @@ void setup() {
   pinMode(PIN_RFID_ENABLE, OUTPUT); // RFID enable
   pinMode(PIN_DOOR_SENSOR_ON, OUTPUT); // On signal
   pinMode(PIN_DOOR_SENSOR_SENSE, INPUT);
-  
+
   digitalWrite(PIN_RFID_ENABLE, LOW); // Enable the RFID Reader.
   digitalWrite(PIN_DOOR_CONTROL, LOW); // Don't open the door.
   digitalWrite(PIN_DOOR_SENSOR_ON, HIGH); // Push out 5v
-  
+
   Serial.begin(9600);
   RFIDReader.begin(2400);
   BlueTooth.begin(19200); //open serial software
-  
+
   RFIDReader.listen();
-  
+
   door.attach(PIN_SERVO_SIGNAL);
   door.write(160);
   delay(2000);
   door.detach();
-  
+
   delay(1000);
   BlueTooth.write("AT\r");
-  
-  
 }
 
 void loop() {
-  if(RFIDReader.available()) {
-     if (RFIDReader.read() == 10) { // start bit.
-       bytesread = 0; // reset read state.
-       tag = "";
-       while (bytesread < 10) { // Loop until we get the full tag data.
-         if (RFIDReader.available()) { // Have we gotten the next bit?
-           val = RFIDReader.read(); // Get the next bit.
+  if (RFIDReader.available()) {
+    int nextByte = RFIDReader.read();
 
-           if (val == 10 || val == 13) { // If we get a start or end bit -- dive out and stop trying to get more bits.
-             break; 
-           }
-           
-           tag.concat(val); // store the tag bit.
-           bytesread++;
-         }
-       }
-       
-       if (bytesread == 10) { // got full tag length.
-         digitalWrite(PIN_RFID_ENABLE, HIGH); // Stop reading rfid input.
-         
-         BlueTooth.println("Tag " + tag);
-         
-         processTag();
-         
-         digitalWrite(PIN_RFID_ENABLE, LOW); // read RFID input again.
-       }
-     }
+    if (nextByte == 13) { // End of tag.
+      isReadingTag = false; // Done reading.
+
+      if (bytesread == 10) { // Make sure we got 10 bytes.
+        digitalWrite(PIN_RFID_ENABLE, HIGH); // Stop reading rfid input.
+        processTag();
+        digitalWrite(PIN_RFID_ENABLE, LOW); // read RFID input again.
+      }
+
+    } else if (nextByte == 10) { // Tag start.
+      isReadingTag = true;
+      bytesread = 0;
+      tag = "";
+
+    } else if (isReadingTag) { // If this wasn't the start or the end -- and we had a start, collect the bytes.
+      tag.concat(nextByte);
+      bytesread++;
+    }
   }
-  
+
   int door_state = digitalRead(PIN_DOOR_SENSOR_SENSE);
-//  BlueTooth.println(door_state == HIGH ? "Door state: HIGH" : "Door state: LOW");
-  if (isOpen == 1 && door_state == HIGH) {
-    delay(1000);
-    // Check that the door is still closed (effectively debouncing).
-    if (digitalRead(PIN_DOOR_SENSOR_SENSE) == HIGH) {
+
+  if (door_state != lastDoorState) {
+    lastDoorDebounceTime = millis();
+  }
+
+  if ((millis() - lastDoorDebounceTime) > doorDebounceDelay) {
+    if (isOpen == 1 && door_state == HIGH) { // Door was last open, now apparently closed.
+      delay(900);
       lockDoor();
       isOpen = 0;
-    }
-  } else if (door_state == LOW) {
-    // Wait to "de-bounce"
-    delay(100);
-    // Check again and proclaim OPEN if still really open.
-    if (digitalRead(PIN_DOOR_SENSOR_SENSE) == LOW) {
+    } else if (door_state == LOW) {
       isOpen = 1;
     }
   }
+
+  lastDoorState = door_state;
 }
 
 void processTag() {
@@ -117,28 +114,28 @@ void processTag() {
   if (isValid) {
     openDoor();
   } else {
-    BlueTooth.println("DENIED"); 
+    BlueTooth.println("DENIED");
   }
 }
 
 void openDoor() {
   BlueTooth.println("OPEN DOOR");
   door.attach(PIN_SERVO_SIGNAL);
-  
+
   // Open
   door.write(35);
   delay(2000);
-  
+
   door.detach();
 }
 
 void lockDoor() {
   BlueTooth.println("LOCK DOOR");
   door.attach(PIN_SERVO_SIGNAL);
-  
+
   // Close
   door.write(160);
   delay(2000);
-  
+
   door.detach();
 }
